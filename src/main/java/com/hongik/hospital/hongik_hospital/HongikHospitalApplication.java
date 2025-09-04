3038002d5b0325b7845e81953a03955b0f5616a9
stingmon <code.stingmon@gmail.com>
2025-09-04 18:55:04 +0900

    Init: create project

---
+++ src/main/java/com/hongik/hospital/hongik_hospital/HongikHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.hospital.hongik_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikHospitalApplication.class, args);
+	}
+
+}
