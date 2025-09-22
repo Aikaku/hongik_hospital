f331b84c8a8288e6c428d1870dfbc7856a4f8c80
stingmon <code.stingmon@gmail.com>
2025-09-22 18:25:51 +0900

    Feat: add createMethod and businessMethod

--- src/main/java/com/hongik/hospital/hongik_hospital/domain/Reservation.java
+++ src/main/java/com/hongik/hospital/hongik_hospital/domain/Reservation.java
@@ -31,4 +31,25 @@ public class Reservation {
 
     @Column(nullable = false)
     private LocalDateTime reserveTime;
+
+    //=====생성 메서드=====//
+
+    public static Reservation createReservation(Patient patient, Doctor doctor, LocalDateTime when) {
+        Reservation reservation = new Reservation();
+
+        reservation.setStatus(ReserveStatus.RESERVE);
+        reservation.setPatient(patient);
+        reservation.setDoctor(doctor);
+        reservation.setReserveTime(when);
+
+        return reservation;
+    }
+
+    //=====비지니스 로직=====//
+    public void cancel() {
+        if (this.getStatus() == ReserveStatus.COMP) {
+            throw new IllegalStateException("이미 진료가 완료된 예약입니다.");
+        }
+        this.setStatus(ReserveStatus.CANCEL);
+    }
 }
